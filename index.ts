43022806085b6acf460ae97af8834ddd49772e2c
Oscar <o_fonte@hotmail.com>
2024-05-02 16:55:21 +0200

    Insertando con prisma el 1er dato

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!");
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+const result = await prisma.technicians.create({
+    data: {
+        firstName: "Pepito",
+        lastName: "DelosPalotes",
+    }
+})
+console.log(result);
