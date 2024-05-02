0a9e865ded87fd1fe440c0a420186a07939e5604
Oscar <o_fonte@hotmail.com>
2024-05-02 17:21:42 +0200

    Arreglando función newTechnician

--- scr/technician.ts
+++ scr/technician.ts
@@ -3,8 +3,8 @@ import { db } from "./db";
 export const newTechnician = async (firstName: string, lastName: string) => {
   const result = await db.technicians.create({
     data: {
-      firstName: "Pepito",
-      lastName: "DelosPalotes",
+      firstName,
+      lastName,
     },
   });
   return result;
